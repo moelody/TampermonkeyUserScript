e274c85be93df251d53662a0967f8b6974c455bc
moelody <yfsmallmoon@gmail.com>
2023-01-27 12:58:09 +0800

    init

---
+++ first.user.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: moelody yfsmallmoon@gmail.com
+ * @Date: 2023-01-27 12:15:21
+ * @LastEditors: moelody yfsmallmoon@gmail.com
+ * @LastEditTime: 2023-01-27 12:53:01
+ * @FilePath: \Tampermonkey\first.user.js
+ * @Description:
+ *
+ * Copyright (c) 2023 by moelody yfsmallmoon@gmail.com, All Rights Reserved.
+ */
+// ==UserScript==
+// @name         第一个油猴脚本
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+;(function () {
+    "use strict"
+
+    // Your code here...
+    console.log("Hello World")
+
+    // alert("Hello World");
+})()
